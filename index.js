e5ef3702a0e26d3767808d95e3c2920b613e349e
MariaDuda <mariabevenuto062@gmail.com>
2022-07-05 16:34:14 -0300

    logar work well

--- index.js
+++ index.js
@@ -25,14 +25,33 @@ app.post("/cadastrarUser" , async (req, res)=>{
     if(validar.error){
         return res.status(400).send("formato incorreto")
     }
+    const emailExistente = await db.collection("users_jwt").findOne({email})
+    if(emailExistente){
+        return res.send("email existente")
+    }
     await db.collection("users_jwt").insertOne({email , senha})
     return res.status(200).send("OK")
 })
 
-app.get("/oi" , (req , res)=>{
-    res.send("oi")
+app.post("/logar",async (req, res) => {
+    const { email, senha } = req.body
+    const userSchema = joi.object({
+        email: joi.string().email().required(),
+        senha: joi.string().required()
+    })
+    console.log(email, senha)
+    const validar = userSchema.validate({ email, senha })
+    if(validar.error){
+        return res.status(400).send("formato incorreto")
+    }
+    const usuarioExistente = await db.collection("users_jwt").findOne({email})
+    if(!usuarioExistente){
+        return res.send("email ou senha incorretos")
+    }
+    return res.send("foi")
+   
 })
 const PORT = 3580
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`entrei ${PORT}`)
 })
